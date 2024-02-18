ad005b72279639ae3a798cab1498fe50d87615f3
OPUS <sublymus@gmail.com>
2024-02-18 15:37:53 +0300

    init postgres

--- env.ts
+++ env.ts
@@ -1,16 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Validating Environment Variables
-|--------------------------------------------------------------------------
-|
-| In this file we define the rules for validating environment variables.
-| By performing validation we ensure that your application is running in
-| a stable environment with correct configuration values.
-|
-| This file is read automatically by the framework during the boot lifecycle
-| and hence do not rename or move this file to a different location.
-|
-*/
 
 import Env from '@ioc:Adonis/Core/Env'
 
@@ -25,13 +12,13 @@ export default Env.rules({
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
 	//
 	DB_CONNECTION: Env.schema.string(),
-	//
-    // PG_HOST: Env.schema.string({ format: 'host' }),
-    // PG_PORT: Env.schema.number(),
-    // PG_USER: Env.schema.string(),
-    // PG_PASSWORD: Env.schema.string.optional(),
-    // PG_DB_NAME: Env.schema.string(),
-	//
+	
+    PG_HOST: Env.schema.string({ format: 'host' }),
+    PG_PORT: Env.schema.number(),
+    PG_USER: Env.schema.string(),
+    PG_PASSWORD: Env.schema.string.optional(),
+    PG_DB_NAME: Env.schema.string(),
+	
 	FRONT_ORIGINE: Env.schema.string(),
 	FRONT_END_HOME: Env.schema.string(),
 	FRONT_END_CREATE_USER: Env.schema.string(),
